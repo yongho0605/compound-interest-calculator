cc9f20aa0bc699b961faba0e4c3df63365135109
장용호 <yongho.jang@datamaker.io>
2024-06-18 17:51:33 +0900

    html meta SEO setting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,19 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   ssr: false,
+  app: {
+    head: {
+      title: '복리 이자 계산기',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'description', content: '월배당 주식투자를 가정한 월 복리 이자 계산기' },
+        { property: 'og:title', content: '복리 이자 계산기' },
+        { property: 'og:type', content: 'website' },
+        { property: 'og:description', content: '월배당 주식투자를 가정한 월 복리 이자 계산기' },
+        { property: 'og:image', content: '/img/og-image.png' }
+      ]
+    }
+  },
   modules: ['@nuxtjs/tailwindcss']
 })
