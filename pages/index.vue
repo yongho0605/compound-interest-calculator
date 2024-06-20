c306da914c3dcf9cb6d7299270f5802262e5bf19
장용호 <yongho.jang@datamaker.io>
2024-06-20 09:44:58 +0900

    App version update 0.1.2

--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,31 @@
 <template>
-  <div class="flex h-[40vh] flex-col justify-around gap-4">
+  <div class="flex h-[40vh] flex-col justify-around gap-4 text-slate-800">
     <h1 class="text-4xl">CIRC (Compound Interest Rate Calculator) - 복리 이자 계산기</h1>
-    <div class="flex h-[30vh]">
-      <Input v-model="calculateTarget.currentInvestmentAmount" type="number" suffix="만원" min="1">
-        현재 투자 금액
-      </Input>
-      <Input v-model="calculateTarget.monthlyInvestment" type="number" suffix="만원" min="1">월 투자금액</Input>
-      <Input v-model="calculateTarget.InterestRate" type="number" suffix="%">복리 이자율</Input>
-      <Input v-model="calculateTarget.months" type="number" min="1">
+    <InputGroup v-model="investmentInfo" class="flex h-[30vh]">
+      <Input name="initialInvestmentAmount" type="number" suffix="만원" min="1">초기 투자 금액</Input>
+      <Input name="monthlyInvestment" type="number" suffix="만원" min="1">월 투자금액</Input>
+      <Input name="InterestRate" type="number" suffix="%">복리 이자율</Input>
+      <Input name="months" type="number" min="1">
         투자 기간
         <template #suffix>
           <Input
-            v-model="calculateTarget.InterestUnit"
+            name="durationUnit"
             input-class="w-full rounded border border-slate-600 p-2"
             type="select"
             :options="interestRateOptions" />
         </template>
       </Input>
-    </div>
+    </InputGroup>
     <div>
-      <div v-if="data.length" class="flex w-full justify-between">
-        <p>(본 정보는 원금 손실을 가정하지 않은, 원금이 그대로 유지된다는 가정하에 계산된 수치입니다.)</p>
+      <div v-show="data?.length" class="flex w-full justify-between">
+        <p>
+          (본 정보는 원금이 그대로 유지된다는 가정 하에 월마다 나오는 배당금을 TR(total return)식으로 재투자하여 산출된
+          급액입니다.)
+        </p>
+        <span class="mr-2 text-lg font-bold text-red-700">적용된 세율: {{ Tax.WITHHOLDING_TAX_RATE }}%</span>
       </div>
-      <table class="w-full text-center text-xl">
-        <tr v-if="data.length" class="grid grid-cols-4 border-t border-slate-700">
+      <table v-show="data?.length" class="w-full text-center text-xl">
+        <tr class="grid grid-cols-4 border-t border-slate-700">
           <th class="border-r border-slate-700"></th>
           <th class="border-r border-slate-700">투자 금액</th>
           <th class="border-r border-slate-700">배당금</th>
@@ -37,7 +39,7 @@
         </tr>
       </table>
     </div>
-    <div class="flex w-full gap-4 text-xl font-bold">
+    <div v-if="total" class="flex w-full gap-4 text-xl font-bold">
       <span v-if="total.withholdingTax > 0">총 원천징수 세액: {{ currencyFormatter(total.withholdingTax) }}</span>
 
       <span v-if="total.principalInvestment">총 투자 원금: {{ currencyFormatter(total.principalInvestment) }}</span>
@@ -50,27 +52,36 @@
 </template>
 
 <script setup>
-import { investmentPeriodUnits } from '~/constant.js'
+import { InvestmentPeriodUnits, Tax } from '~/constant.js'
 import { truncateTwoDecimalPlaces, convertToKoreaCurrency, currencyFormatter } from '~/utils/calculator.js'
-const { MONTHLY, YEARLY } = investmentPeriodUnits
+const { MONTHLY, YEARLY } = InvestmentPeriodUnits
 
-const calculateTarget = reactive({
-  currentInvestmentAmount: 0,
+const investmentInfo = reactive({
+  initialInvestmentAmount: 0,
   monthlyInvestment: 0,
   InterestRate: 0,
-  InterestUnit: MONTHLY,
-  months: 1
+  durationUnit: MONTHLY,
+  months: 1,
+  ISA: false
 })
 const data = ref([])
-const total = ref({ withholdingTax: 0 })
+const total = ref({})
 
 const interestRateOptions = [
   { key: '개월', value: MONTHLY },
   { key: '년', value: YEARLY }
 ]
 
-watch(calculateTarget, (value) => {
-  const result = calculator(value)
+const numberInputGuard = (value) => (!value ? 0 : value)
+
+watch(investmentInfo, (value) => {
+  for (const key in value) {
+    value[key] = numberInputGuard(value[key])
+  }
+
+  const result = calculator({ ...value, withholdingTaxRate: Tax.WITHHOLDING_TAX_RATE })
+  if (!result) return false
+
   data.value = result.data
   total.value = result.total
 })
