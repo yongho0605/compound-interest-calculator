e101ec96ee56a5132422710774b105b67bfaeec2
장용호 <yongho.jang@datamaker.io>
2024-06-22 19:28:43 +0900

    Create CheckboxInputs.vue component

---
+++ components/forms/CheckboxInputs.vue
@@ -0,0 +1,43 @@
+<template>
+  <fieldset class="space-y-5">
+    <div v-for="checkbox in checkboxList" :key="checkbox.key" class="relative flex items-start">
+      <div class="flex h-6 items-center">
+        <input
+          :id="checkbox.name"
+          :name="checkbox.name"
+          type="checkbox"
+          :disabled="checkbox.disabled"
+          class="h-4 w-4 rounded border-gray-300 text-blue-600 focus:ring-blue-600 disabled:bg-gray-100" />
+      </div>
+      <div class="ml-3 text-sm leading-6">
+        <label :for="checkbox.name" class="font-medium" :class="checkbox.disabled ? 'text-gray-300' : 'text-gray-900'">
+          {{ checkbox.title }}
+        </label>
+        <p :class="checkbox.disabled ? 'text-gray-200' : 'text-gray-500'">{{ checkbox.description }}</p>
+      </div>
+    </div>
+  </fieldset>
+</template>
+
+<script setup>
+defineProps({
+  checkboxList: {
+    type: Array,
+    required: true
+  }
+})
+</script>
+
+<style scoped>
+.p-accordion {
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+}
+
+.p-accordion-header-link {
+  display: flex;
+  align-items: center;
+  gap: 20px;
+}
+</style>
